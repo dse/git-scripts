571e41dbc622ba757ebf3a35c6c85784bb487edd
Darren Embry <dse@webonastick.com>
2018-09-13 21:30:32 -0400

    additional functions for new script git-merge-into.

--- share/git-scripts/git-scripts.sh
+++ share/git-scripts/git-scripts.sh
@@ -9,3 +9,31 @@ run () {
     fi
     "$@"
 }
+
+croak () {
+    sed 's/^[ '$'\t'']*|//' >&2
+
+    local exitstatus="$1"; shift
+    local source="$1"; shift
+
+    if [[ "${exitstatus}" == "" ]] ; then
+        exitstatus=1
+    else true ; fi
+
+    local current
+    current="$(git rev-parse --abbrev-ref HEAD)"
+
+    if [[ "${source}" != "" ]] && [[ "${current}" != "${source}" ]] ; then
+        >&2 echo ""
+        >&2 echo "    YOU ARE STILL IN BRANCH ${current}."
+    else true ; fi
+
+    >&2 echo
+    >&2 echo "Exiting."
+    exit "${exitstatus}"
+}
+
+sendoff () {
+    sed 's/^[ '$'\t'']*|//' >&2
+    exit 0
+}
